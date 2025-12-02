acc9b60bb07990adca6890ff70973b81ee9d2854
Tomás Lopes <161335939+lopest-f@users.noreply.github.com>
2025-12-02 23:44:57 +0000

    Merge pull request #1 from lopest-f/patch-1
    Day 02

--- day-02/solution.py
+++ day-02/solution.py
@@ -1,4 +1,22 @@
-data = []
-with open(0, "r") as f:
-    data = [line.strip() for line in f.readlines()]
+ranges = []
+p1 = p2 = 0
 
+with open(0, "r") as f:
+    ranges = [(int(x), int(y)) for x, y in [r.split("-") for r in f.readline().strip().split(",")]]
+
+def is_invalid_p1(s):
+    return s[: len(s) // 2] == s[len(s) // 2 :]
+
+def is_invalid_p2(s):
+    for group_len in range(len(s) // 2):
+        groups = [s[i : i + group_len + 1] for i in range(0, len(s), group_len + 1)]
+        if all(g == groups[0] for g in groups): return True
+    return False
+
+for start, end in ranges:
+    for i in range(start, end + 1):
+        if is_invalid_p1(str(i)): p1 += i
+        if is_invalid_p2(str(i)): p2 += i
+
+print("Part 1:", p1)
+print("Part 2:", p2)
