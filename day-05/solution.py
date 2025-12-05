16059e1815e1fb6d5713b9d0bfd295c65c2cfe06
Tomás Lopes <tomaslopes.dev@gmail.com>
2025-12-05 15:47:48 +0000

    refactored day 05

--- day-05/solution.py
+++ day-05/solution.py
@@ -1,18 +1,16 @@
 with open(0, "r") as f:
     lines = f.readlines()
-    ranges, ingredients = (
-        [list(map(int, line.split("-"))) for line in lines if "-" in line],
-        [int(line) for line in lines if line.strip() != "" and "-" not in line],
-    )
 
-fresh = {
-    ing for ing in ingredients
-    if any(a <= ing <= b for a, b in ranges)
-}
+ranges, ingredients = (
+    sorted([list(map(int, line.split("-"))) for line in lines if "-" in line]),
+    [int(line) for line in lines if line.strip() != "" and "-" not in line],
+)
+
+fresh = { ing for ing in ingredients
+    if any(a <= ing <= b for a, b in ranges) }
 
 merged = []
-intervals = sorted(ranges)
-for start, end in intervals:
+for start, end in ranges:
     if merged == [] or start > merged[-1][1]:
         merged.append([start, end])
     else:
