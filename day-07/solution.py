12504f43fbded52e42812f81ab31f192546d3e25
Tomás Lopes <tomaslopes.dev@gmail.com>
2025-12-07 19:37:46 +0000

    refactored day 07

--- day-07/solution.py
+++ day-07/solution.py
@@ -1,28 +1,16 @@
-data = []
-p1 = 0
-
 with open(0, "r") as f:
     data = [line.strip() for line in f.readlines()]
 
-start = len(data[0]) // 2
-pos = {start}
-for line in data[1:]:
-  temp = set()
-  for p in pos:
-      if line[p] == "^":
-          p1 += 1
-          temp.update({p - 1, p + 1})
-      else:
-          temp.add(p)
-  pos = temp
+pos = {len(data[0])//2: 1}
+p1 = 0
 
-pos = {start: 1}
-for line in data[1:]:
+for line in data[2:]:
   temp = {}
   for p, count in pos.items():
       if line[p] == "^":
           temp[p - 1] = temp.get(p - 1, 0) + count
           temp[p + 1] = temp.get(p + 1, 0) + count
+          p1 += 1
       else:
           temp[p] = temp.get(p, 0) + count
   pos = temp
