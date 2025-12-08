7c4f2b892ce00efe16c1b2eb7bb8326f122e4bda
Tomás Lopes <tomaslopes.dev@gmail.com>
2025-12-08 17:21:59 +0000

    refactored day 08

--- day-08/solution.py
+++ day-08/solution.py
@@ -1,8 +1,5 @@
 import math
 
-
-p1 = p2 = 0
-
 with open(0, "r") as f:
     data = [line.strip().split(",") for line in f.readlines()]
 
@@ -17,7 +14,7 @@ for i, (x, y, z) in enumerate(data):
 dists = dict(sorted(dists.items(), key=lambda item: item[1]))
 
 circuits = []
-for (x, y, z, xx, yy, zz), dist in list(dists.items())[:1000]:
+for i, ((x, y, z, xx, yy, zz), _) in enumerate(list(dists.items())):
     first_circuit = None
     second_circuit = None
     for circuit in circuits:
@@ -34,33 +31,9 @@ for (x, y, z, xx, yy, zz), dist in list(dists.items())[:1000]:
     elif first_circuit != second_circuit:
         first_circuit.update(second_circuit)
         circuits.remove(second_circuit)
-
-circuits = []
-for i, ((x, y, z, xx, yy, zz), dist) in enumerate(list(dists.items())):
-    first_circuit = None
-    second_circuit = None
-    for circuit in circuits:
-        if (x, y, z) in circuit:
-            first_circuit = circuit
-        if (xx, yy, zz) in circuit:
-            second_circuit = circuit
-    if first_circuit and not second_circuit:
-        first_circuit.add((xx, yy, zz))
-    elif not first_circuit and second_circuit:
-        second_circuit.add((x, y, z))
-    elif not first_circuit and not second_circuit:
-        circuits.append(set([(x, y, z), (xx, yy, zz)]))
-    elif first_circuit != second_circuit:
-        first_circuit.update(second_circuit)
-        circuits.remove(second_circuit)
-
+    if i == 1000:
+      circuits = sorted(circuits, key=lambda c: len(c), reverse=True)
+      print("Part 1:", math.prod([len(c) for c in circuits[:3]]))
     if len(circuits) == 1 and len(circuits[0]) == len(data):
-        print(x, xx, i)
-        p2 = int(x) * int(xx)
+        print("Part 2:", int(x) * int(xx))
         break
-
-circuits = sorted(circuits, key=lambda c: len(c), reverse=True)
-print("Part 1:", math.prod([len(c) for c in circuits[:3]]))
-print("Part 2:", p2)
-
-# 1440 too low
