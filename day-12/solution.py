be3e8e04f85da00d02e6c7c83765a21bb767b544
Tomás Lopes <tomaslopes.dev@gmail.com>
2025-12-12 06:01:11 +0000

    refactored day 12

--- day-12/solution.py
+++ day-12/solution.py
@@ -1,19 +1,6 @@
-p1 = p2 = 0
-
 with open(0, "r") as f:
     data = [x.split("\n") for x in [line.strip() for line in f.read().split("\n\n")]]
     shapes = ["".join(x[1:]).count("#") for x in data[:-1]]
-    regions = [{"dimensions": list(map(int, x.split(":")[0].strip().split("x"))), "points": [int(point.strip()) for point in x.split(":")[1].strip().split()]} for x in data[-1]]
+    regions = [{"dimensions": tuple(map(int, x.split(":")[0].strip().split("x"))), "points": [int(point.strip()) for point in x.split(":")[1].strip().split()]} for x in data[-1]]
 
-for region in regions:
-    height, width = region["dimensions"]
-    points = region["points"]
-    c = 0
-    for quant, shape in zip(points, shapes):
-        c += quant * shape
-
-    print(c, height * width)
-    if c < height * width:
-        p1 += 1
-print("Part 1:", p1)
-print("Part 2:", p2)
+print("Part 1:", sum(1 for region in regions if sum(quant * shapes[i] for i, quant in enumerate(region["points"])) < region["dimensions"][0] * region["dimensions"][1]))
